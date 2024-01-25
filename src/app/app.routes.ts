67a33b2e6b8d70b6745e177f5a4456aa56222ac9
FellipeRabelo <josefelliperabelopds@gmail.com>
2024-01-25 10:54:26 -0300

    Atividade-1-implementar-angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+export default routes;
